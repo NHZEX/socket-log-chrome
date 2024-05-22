6218fc9c5252d87e685f9e0107e801adea31bfa0
auooru <auooru@outlook.com>
2024-05-22 14:59:11 +0800

    update: 修复 store 值传递并优化代码

--- src/popup/app.vue
+++ src/popup/app.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="wrapper clearfix">
     <div class="title">
-      SocketLog 设置 （状态：<span>{{ data.stateMsg }}</span>）
+      SocketLog 设置 （状态：<span>{{ stateMessage }}</span>）
       <div class="help">
         <a href="https://github.com/NHZEX/socket-log-chrome" title="帮助" target="_blank">
           <img src="@/assets/image/help_16.png" alt="help"/>
@@ -13,17 +13,17 @@
         <div>
           <section>
             监听协议：
-            <input type="radio" v-model="data.address.tls" :value="false" name="protocol">ws
-            <input type="radio" v-model="data.address.tls" :value="true" name="protocol">wss
+            <input type="radio" v-model="address.tls" :value="false" name="protocol">ws
+            <input type="radio" v-model="address.tls" :value="true" name="protocol">wss
           </section>
-          监听主机： <input v-model="data.address.host" title="监听主机"/> <br />
-          监听端口： <input v-model.number="data.address.port" title="监听端口"/> <br />
-          监听路径： <input v-model.trim="data.address.path" title="监听路径"/> <br />
-          连接地址： <input :value="data.displayUrl" title="监听地址" readonly style="background-color: rgba(66,66,66,0.5)"/> <br/>
-          ClientID： <input v-model="data.clientId" title="客户ID"/> <br/>
+          监听主机： <input v-model="address.host" title="监听主机"/> <br />
+          监听端口： <input v-model.number="address.port" title="监听端口"/> <br />
+          监听路径： <input v-model.trim="address.path" title="监听路径"/> <br />
+          连接地址： <input :value="displayUrl" title="监听地址" readonly style="background-color: rgba(66,66,66,0.5)"/> <br/>
+          ClientID： <input v-model="clientId" title="客户ID"/> <br/>
         </div>
         <div style="max-width: 100px">
-          开启：<input type="checkbox" v-model="data.enable" title="服务开关"><br />
+          开启：<input type="checkbox" v-model="enableListen" title="服务开关"><br />
           <button type="button" @click="onSave">保 存 / 重 连</button>
         </div>
       </div>
@@ -33,7 +33,7 @@
       <label>允许监听域名：</label>
       <div style="display: flex; justify-content: space-around">
         <textarea
-            v-model.trim="data.allowHosts"
+            v-model.trim="allowRulesEdit"
             autocomplete="off"
             spellcheck="false"
             rows="10"
@@ -61,102 +61,92 @@
   </div>
 </template>
 
-<script>
+<script setup>
 
 import { reactive, computed, onMounted, onUnmounted, ref, watch, toRef } from 'vue'
 import { usePopupStore } from 'src/stores/popupStore'
 import { restartConnection } from 'src/helper'
 import { getAllowHostRules, setAllowHosts } from "../storage";
+import { storeToRefs } from "pinia";
 
-export default {
-  name: 'App',
-  components: {
+const popupStore = usePopupStore()
+const popupStoreRefs = storeToRefs(popupStore)
+
+popupStore.loadStorageData()
+
+const allowHosts = ref([])
+const enabledRuleCount = ref(0)
+
+const address = popupStoreRefs.address
+const clientId = popupStoreRefs.clientId
+const enableListen = popupStoreRefs.enableListen
+
+const protocol = computed(() => popupStore.address.tls ? 'wss' : 'ws')
+const displayUrl = computed(() => `${protocol.value}://${address.value.host}:${address.value.port}${address.value.path}`)
+
+const stateMessage = computed(() => popupStore.stateMsg)
+
+// 域名配置
+const allowRulesEdit = computed({
+  get: () => allowHosts.value
+      .join('\n'),
+  set: val => {
+    allowHosts.value = String(val)
+        .split('\n')
+        .map(line => line.trim())
+        .filter(line => !!line)
   },
-  setup() {
-    const popupStore = usePopupStore()
+})
 
-    popupStore.loadStorageData()
+watch(allowHosts, (val) => {
+  console.log('allowRules', val)
+})
 
-    const allowHosts = ref([])
-    const enabledRuleCount = ref(0)
+const onSave = () => {
+  popupStore.saveStorageData()
+  restartConnection();
+}
 
-    const data = reactive({
-      stateMsg: computed(() => popupStore.stateMsg),
-      address: toRef(() => popupStore.address),
-      clientId: toRef(() => popupStore.clientId),
-      enable: toRef(() => popupStore.enableListen),
-      protocol: computed(() => popupStore.address.tls ? 'wss' : 'ws'),
-      displayUrl: computed(() => `${data.protocol}://${data.address.host}:${data.address.port}${data.address.path}`),
-      // 域名配置
-      allowHosts: computed({
-        get: () => allowHosts.value
-            .join('\n'),
-        set: val => {
-          allowHosts.value = String(val)
-              .split('\n')
-              .map(line => line.trim())
-              .filter(line => !!line)
-        },
-      }),
-    })
-
-    watch(allowHosts, (val) => {
-      console.log('allowHosts', val)
-    })
-
-    const onSave = () => {
-      popupStore.saveStorageData()
-      restartConnection();
-    }
-
-    const onSaveAllowHosts = async () => {
-      try {
-        allowHosts.value = await setAllowHosts(allowHosts.value)
-      } catch (e) {
-        alert(`规则输入有误，请重新检查 \n${e}`)
-      }
-    }
-
-    const onMessage = ({ status_message: statusMessage }) => {
-      if (statusMessage === undefined) {
-        return
-      }
-      const { newValue, oldValue } = statusMessage
-      console.log('session.onChanged', newValue, oldValue)
-      if (newValue !== oldValue) {
-        popupStore.stateMsg = newValue
-      }
-    }
-
-    const refreshEnableRuleCount = async () => {
-      enabledRuleCount.value = (await chrome.declarativeNetRequest.getDynamicRules()).length;
-    }
-    let _tidRefreshEnableRuleCount = null
-
-    onMounted(async () => {
-      chrome.storage.session.onChanged.addListener(onMessage);
-      allowHosts.value = await getAllowHostRules();
-      await refreshEnableRuleCount()
-
-      _tidRefreshEnableRuleCount = setInterval(async () => await refreshEnableRuleCount(), 1000)
-    })
-    onUnmounted(() => {
-      chrome.storage.session.onChanged.removeListener(onMessage)
-
-      if (_tidRefreshEnableRuleCount) {
-        _tidRefreshEnableRuleCount = null
-        clearInterval(_tidRefreshEnableRuleCount)
-      }
-    })
-
-    return {
-      data,
-      enabledRuleCount,
-      onSave,
-      onSaveAllowHosts,
-    }
+const onSaveAllowHosts = async () => {
+  try {
+    allowHosts.value = await setAllowHosts(allowHosts.value)
+  } catch (e) {
+    alert(`规则输入有误，请重新检查 \n${e}`)
   }
 }
+
+const onMessage = ({ status_message: statusMessage }) => {
+  if (statusMessage === undefined) {
+    return
+  }
+  const { newValue, oldValue } = statusMessage
+  console.log('session.onChanged', newValue, oldValue)
+  if (newValue !== oldValue) {
+    popupStore.stateMsg = newValue
+  }
+}
+
+const refreshEnableRuleCount = async () => {
+  enabledRuleCount.value = (await chrome.declarativeNetRequest.getDynamicRules()).length;
+}
+let _tidRefreshEnableRuleCount = null
+
+onMounted(async () => {
+  chrome.storage.session.onChanged.addListener(onMessage);
+  allowHosts.value = await getAllowHostRules();
+  await refreshEnableRuleCount()
+
+  _tidRefreshEnableRuleCount = setInterval(async () => await refreshEnableRuleCount(), 1000)
+})
+onUnmounted(() => {
+  chrome.storage.session.onChanged.removeListener(onMessage)
+
+  if (_tidRefreshEnableRuleCount) {
+    _tidRefreshEnableRuleCount = null
+    clearInterval(_tidRefreshEnableRuleCount)
+  }
+})
+
 </script>
 
 <style>

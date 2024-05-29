c2a486fede796957f0dff355a49d09bac511c5e7
auooru <auooru@outlook.com>
2024-05-29 13:54:40 +0800

    update: 更新依赖项

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,19 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
 import pluginVue from "eslint-plugin-vue";
-
 
 export default [
     {
         languageOptions: {
             globals: {
-                ...globals.node,
                 ...globals.webextensions,
-            },
+                ...globals.node
+            }
         }
     },
     pluginJs.configs.recommended,
+    ...tseslint.configs.recommended,
     ...pluginVue.configs["flat/essential"],
     {
         rules: {
